bfcc8d1c01a47de3bf6d52c9fa7b2c444ce84b5c
Jason Porta <jason.porta@gmail.com>
2026-05-17 17:40:16 -0400

    Add files via upload

---
+++ Emails/send_gmail_with_attachments.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+sender_email = "jason.porta@gmail.com"
+sender_password = "udgu hfhn zfgt gpfw"
+recipient_email = "jason.porta@gmail.com"
+subject = "Hello from Python"
+body = "with attachment"
+
+
+with open("attachment.txt", "rb") as attachment:
+    # Add the attachment to the message
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+encoders.encode_base64(part)
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= 'attachment.txt'",
+)
+
+message = MIMEMultipart()
+message['Subject'] = subject
+message['From'] = sender_email
+message['To'] = recipient_email
+html_part = MIMEText(body)
+message.attach(html_part)
+message.attach(part)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+   server.login(sender_email, sender_password)
+   server.sendmail(sender_email, recipient_email, message.as_string())
